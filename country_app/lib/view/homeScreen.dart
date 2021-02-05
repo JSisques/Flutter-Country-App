9831d044749fd54ef93fdba8cd88580e639a0d91
Javi Plaza <javierplazasisques@gmail.com>
2021-02-05 15:15:36 +0100

    05/02/2021

--- country_app/lib/view/homeScreen.dart
+++ country_app/lib/view/homeScreen.dart
@@ -1,6 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
 
 class HomeScreen extends StatelessWidget {
+
+  int selectedIndex = 0;
+  CameraPosition initialPosition = CameraPosition(target: LatLng(26.8206, 30.8025));
+  Completer<GoogleMapController> _controller = Completer();
+
   //Cogemos los datos de la anterior pantalla
   static Route<dynamic> route() {
     return MaterialPageRoute(
@@ -9,13 +18,38 @@ class HomeScreen extends StatelessWidget {
   }
 
   //Indicamos que el mensaje es obligatorio al entrar en esta ventana
-  const HomeScreen({Key key}) : super(key: key);
+  HomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Inicio'),
+        centerTitle: false,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Hola",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: "Add",
+          ),
+        ],
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.red,
+      ),
+      body: GoogleMap(
+        initialCameraPosition: initialPosition,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);},
       ),
     );
   }
