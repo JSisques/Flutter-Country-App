ebe7296fd22cf53a6ab9e3cb46f8cd52ba18c8ca
Javi Plaza <javierplazasisques@gmail.com>
2021-02-05 23:24:36 +0100

    05/02/2021

---
+++ country_app/lib/model/api.dart
@@ -0,0 +1,10 @@
+import 'package:http/http.dart' as http;
+/*
+Future getData(){
+  Future<http.Response> fetchAlbum() {
+  return http.get(Uri.https('https://restcountries.eu/rest/v2/all',));
+}
+
+}
+
+*/
